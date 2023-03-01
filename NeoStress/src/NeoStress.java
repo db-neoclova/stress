edc7a69a9819a77b0aec880af77b695c0f984a4c
ssauravy <ssauravy@ssauravy-note>
2023-03-02 00:42:57 +0900

    미완

--- NeoStress/src/NeoStress.java
+++ NeoStress/src/NeoStress.java
@@ -1,9 +1,42 @@
+import java.sql.SQLException;
 
-public class NeoStress {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//@SpringBootApplication
+public class NeoStress  {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException  {
+		//SpringApplication.run(NeoStress.class, args);
+		/*
+		[{
+			"mysql-host": "192.168.14.14",
+			"mysql-port": "3306",
+			"mysql-user":"maxscale",
+			"mysql-password":"maxscale",
+			"mysql-db":"sysbench",
+			"threads":"10",
+			"times":"5000"
+		}]
+		 */
+		
+		
 		System.out.println("NeoClova Stress Test Start!!");
+		int numThreads=10;
+		int numLongTimeSecond=5000;
+        
+		StressPool pool = new StressPool(numThreads);
+		for (int i = 0; i < numThreads; i++) {
+        	StressRun task = new StressRun(i, null);
+            pool.execute(task);
+            //System.out.println("----------"+ i);
+        }
+        Thread.sleep(numLongTimeSecond); 
+        System.out.println("NeoClova Stress Test End!!");
+        return;
 	}
 
 }
+
+
+
+
+
